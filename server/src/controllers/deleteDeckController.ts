67bc7b422cd4d84ce0d064e45574962c3dcddc57
Kendrick <devbykendrick@gmail.com>
2024-03-15 16:24:58 -0700

    Updated Vercel for Google

--- server/src/controllers/deleteDeckController.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from "express";
-import Deck from "../models/Deck";
-
-export async function deleteDeckController(req: Request, res: Response) {
-  const deckId = req.params.deckId;
-  const deck = await Deck.findByIdAndDelete(deckId);
-  res.json(deck);
-}
