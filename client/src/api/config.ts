3d4a61534dca6d87631b7d522b44c209a83708cd
Kendrick <devbykendrick@gmail.com>
2024-03-15 16:43:25 -0700

    Updated Vercel for Google #3

--- client/src/api/config.ts
+++ client/src/api/config.ts
@@ -1,7 +1,7 @@
-// export const API_URL = "https://scratchpad-api-three.vercel.app/api/message";
-// export const USER_API_URL = "https://scratchpad-api-three.vercel.app/api/user";
-// export const GOOGLE_API_URL = "https://scratchpad-api-three.vercel.app";
+export const API_URL = "https://scratchpad-api-three.vercel.app/api/message";
+export const USER_API_URL = "https://scratchpad-api-three.vercel.app/api/user";
+export const GOOGLE_API_URL = "https://scratchpad-api-three.vercel.app";
 
-export const API_URL = "http://localhost:8000/api/message";
-export const USER_API_URL = "http://localhost:8000/api/user";
-export const GOOGLE_API_URL = "http://localhost:8000";
+// export const API_URL = "http://localhost:8000/api/message";
+// export const USER_API_URL = "http://localhost:8000/api/user";
+// export const GOOGLE_API_URL = "http://localhost:8000";
