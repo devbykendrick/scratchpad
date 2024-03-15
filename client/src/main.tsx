1df043d9f5db75cd7a69b40ff75433bbb442e282
Kendrick <devbykendrick@gmail.com>
2024-03-15 14:44:17 -0700

    Updated Vercel

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,24 +2,15 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { AuthContextProvider } from "./context/AuthContext";
-import { useAuthContext } from "./hooks/useAuthContext";
 import App from "./app";
 import "./index.css";
-
-const { user } = useAuthContext();
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthContextProvider>
-      {user ? (
-        <GoogleOAuthProvider clientId={`${import.meta.env.GOOGLE_CLIENT_ID}`}>
-          <App />
-        </GoogleOAuthProvider>
-      ) : (
-        <GoogleOAuthProvider clientId={""}>
-          <App />
-        </GoogleOAuthProvider>
-      )}
+      <GoogleOAuthProvider clientId={`${import.meta.env.GOOGLE_CLIENT_ID}`}>
+        <App />
+      </GoogleOAuthProvider>
     </AuthContextProvider>
   </React.StrictMode>
 );
